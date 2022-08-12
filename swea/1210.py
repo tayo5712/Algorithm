cb6804c43fcc6303e3491a92801a83ca2c9a6bbf
tayo5712 <kimty5712@gmail.com>
2022-08-13 02:28:00 +0900

    1210사다리 델타 안쓰고

--- swea/1210.py
+++ swea/1210.py
@@ -2,11 +2,9 @@ import sys
 
 sys.stdin = open("input_1210.txt", "r")
 
-dx = [-1, 1, 0]
-dy = [0, 0, -1]
-
+# 델타 안쓰고
 T = 10
-for tc in range(1, T+1):
+for _ in range(1, T+1):
     N = int(input())
     ladder = [list(map(int, input().split())) for _ in range(100)]
 
@@ -14,19 +12,12 @@ for tc in range(1, T+1):
         if ladder[99][i] == 2:
             x = i
 
-    k = 0
-    y = 99
-    while y > 0:
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < 100 and 0 <= ny < 100 and ladder[ny][nx]:
-            ladder[ny][nx] = 0
-            x = nx
-            y = ny
-            k = 0
-
-        else:
-            k = (k + 1) % 3
-    print('#{} {}'.format(tc, x))
-
+    for y in range(99, 0, -1):
+        if 0 < x <= 99 and ladder[y][x-1] == 1:
+            while 0 < x <= 99 and ladder[y][x-1] == 1:
+                x -= 1
+        elif 0 <= x < 99 and ladder[y][x+1] == 1:
+            while 0 <= x < 99 and ladder[y][x+1] == 1:
+                x += 1
+    print(f'#{N} {x}')
 
