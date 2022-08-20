840fcb44aaf939aa7f9806e51e8fbcc7fcc40fb6
tayo5712 <kimty5712@gmail.com>
2022-08-20 15:01:28 +0900

    달팽이

--- baekjoon/4949.py
+++ baekjoon/4949.py
@@ -1,25 +1,32 @@
-def pack(sentence):
+import sys
+
+while True:
+    stack = []
+    sentence = sys.stdin.readline().rstrip()
+    if sentence == '.':
+        break
+    result = True
     for i in sentence:
         if i == '(' or i == '[':
             stack.append(i)
+
         elif i == ')' or i == ']':
-            if i == ')' and stack[-1] == '(':
+            if not stack:
+                result = False
+                break
+            elif i == ')' and stack[-1] == '(':
                 stack.pop()
             elif i == ']' and stack[-1] == '[':
                 stack.pop()
-            elif i == ')' and stack[-1] == '[':
-                return 'no'
-            elif i == ']' and stack[-1] == '(':
-                return 'no'
-            elif not stack:
-                return 'no'
-    if stack:
-        return 'no'
-    else:
-        return 'yes'
-stack = []
+            else:
+                result = False
+                break
 
-while True:
-    sentence = input()
-        break
-    print(pack(sentence))
+    if result and not stack:
+        print('yes')
+    else:
+        print('no')
+
+
+
+
