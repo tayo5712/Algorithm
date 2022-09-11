987ba2bb3a2107207d07a8434e5d2341487bcc90
tayo5712 <kimty5712@gmail.com>
2022-09-12 00:58:17 +0900

    0912

---
+++ baekjoon/class3/1260-DFS와 BFS.py
@@ -0,0 +1,35 @@
+
+
+def dfs(graph, v, dfs_visited):
+    dfs_visited[v] = 1
+    print(v, end=' ')
+    for i in graph[v]:
+        if not dfs_visited[i]:
+            dfs(graph, i, dfs_visited)
+
+def bfs(graph, v):
+    visited = [0] * (n + 1)
+    q = []
+    q.append(v)
+    visited[v] = 1
+    while q:
+        v = q.pop(0)
+        print(v, end = ' ')
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = 1
+                q.append(i)
+
+n, m ,v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+   a, b = map(int, input().split())
+   graph[a].append(b)
+   graph[b].append(a)
+for i in graph:
+    i.sort()
+
+dfs_visited = [0] * (n + 1)
+dfs(graph, v, dfs_visited)
+print()
+bfs(graph, v)
