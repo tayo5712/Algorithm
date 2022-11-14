ddc8ffbdcb39fc824f238f805dacbe92831fbe3b
tayo5712 <kimty5712@gmail.com>
2022-11-14 21:16:19 +0900

    10809

---
+++ baekjoon/class1/10809-알파벳 찾기.py
@@ -0,0 +1,6 @@
+words = input()
+for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+    if i in words:
+        print(words.index(i), end=' ')
+    else:
+        print(-1, end=' ')
