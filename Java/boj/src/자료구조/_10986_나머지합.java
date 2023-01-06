9c86a40c6baa0654e696f8a7abbddb2e8f18ee1f
tayo5712 <kimty5712@gmail.com>
2023-01-06 23:13:34 +0900

    10986-나머지합(java)

--- Java/boj/src/자료구조/_10986_나머지합.java
+++ Java/boj/src/자료구조/_10986_나머지합.java
@@ -13,24 +13,26 @@ public class _10986_나머지합 {
         int M = Integer.parseInt(st.nextToken());
 
         // 결과 값 담을 변수
-        int result = 0;
+        long result = 0;
 
         // 합배열 만들기
         long[] sumArr = new long[N + 1];
         // 나머지 배열
-        int[] remains = new int[M];
+        long[] remains = new long[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            int A = Integer.parseInt(st.nextToken());
+            long A = Integer.parseInt(st.nextToken());
             sumArr[i] = sumArr[i - 1] + A;
-            int remain = (int) (sumArr[i] % M);
+            long remain = sumArr[i] % M;
             if (remain == 0) {
                 result += 1;
             }
-            remains[remain] += 1;
+            remains[(int) remain] += 1;
         }
-        for (int i = 1; i < M; i++) {
-            result += (remains[i] * (remains[i]-1)) / 2;
+        for (int i = 0; i < M; i++) {
+            if (remains[i] > 1) {
+                result += (remains[i] * (remains[i]-1)) / 2;
+            }
         }
         System.out.println(result);
     }
