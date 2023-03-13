0d6cd79a1d59c8ce54026208b5b7e06a35415925
tayo5712 <kimty5712@gmail.com>
2023-03-13 23:06:36 +0900

    이코테 : 1이 될 ë때까지

--- Python/이코테/그리디/1이 될 때까지.py
+++ Python/이코테/그리디/1이 될 때까지.py
@@ -1,23 +1,10 @@
-N, K = map(int, input().split())
+n, m = map(int, input().split())
 cnt = 0
-# while N > 1:
-#     if N % K == 0:
-#         N = N // K
-#         cnt += 1
-#     else:
-#         N = N - 1
-#         cnt += 1
-#
-# print(cnt)
-
-while True:
-    target = (N // K) * K
-    cnt += (N - target)
-    N = target
-    if N < K:
-        break
-    cnt += 1
-    N //= K
-
-cnt += (N - 1)
+while n != 1:
+    if n % m != 0:
+       n -= 1
+       cnt += 1
+    else:
+        n //= m
+        cnt += 1
 print(cnt)
