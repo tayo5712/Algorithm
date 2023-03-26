36bb3866744370aaf40fae28fa49dc8151eec94c
tayo5712 <kimty5712@gmail.com>
2023-03-26 21:02:20 +0900

    ã이코테 : 성적이 낮은 순서로 학생 출력하기

--- Python/이코테/정렬/성적이 낮은 순서로 학생 출력하기.py
+++ Python/이코테/정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -1,5 +1,19 @@
+"""
+4
+홍길동 95
+이순신 77
+대조영 88
+정약용 99
+"""
+
 n = int(input())
-lst = list(input().split() for _ in range(n))
-chart = sorted(lst, key=lambda score: int(score[1]))
-for i in chart:
-    print(i[0], end = ' ')
+array = [input().split() for _ in range(n)]
+order = sorted(array, key=lambda score: int(score[1]))
+for i in order:
+    print(i[0], end=" ")
+
+# n = int(input())
+# lst = list(input().split() for _ in range(n))
+# chart = sorted(lst, key=lambda score: int(score[1]))
+# for i in chart:
+#     print(i[0], end = ' ')
