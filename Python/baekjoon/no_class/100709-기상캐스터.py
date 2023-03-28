a61b0ccd91f2ba20baeff448ddfd48013e84c2bf
tayo5712 <kimty5712@gmail.com>
2023-03-28 21:39:06 +0900

    boj10709-기상캐스터

---
+++ Python/baekjoon/no_class/100709-기상캐스터.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+mapp = [list(input()) for _ in range(n)]
+result = [[-1 for _ in range(m)] for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if mapp[i][j] == "c":
+            result[i][j] = 0
+        if result[i][j] != -1 and j+1 < m and result[i][j+1] != "c":
+            result[i][j+1] = result[i][j] + 1
+for i in range(n):
+    print(*result[i])
