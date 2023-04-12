a351e38a3134cbc38d5c926ec445ea7bb4190667
tayo5712 <kimty5712@gmail.com>
2023-04-12 18:59:07 +0900

    boj1531-투명

---
+++ Python/baekjoon/no_class/1531-투명.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+mapp = [[0 for _ in range(100)] for _ in range(100)]
+for _ in range(n):
+    ax, ay, bx, by = map(int, input().split())
+    for i in range(ax, bx+1):
+        for j in range(ay, by+1):
+            mapp[i][j] += 1
+
+cnt = 0
+for i in range(100):
+    for j in range(100):
+       if mapp[i][j] > m:
+           cnt += 1
+
+print(cnt)
