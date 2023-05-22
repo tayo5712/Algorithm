1cdcd5eed2cd1903b19406d622fe8138288a984d
tayo5712 <kimty5712@gmail.com>
2023-05-22 23:07:18 +0900

    boj10768-특별한날

---
+++ Python/baekjoon/no_class/10768-특별한날.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+if a == 2 and b < 18:
+    print("Before")
+elif a == 1:
+    print("Before")
+elif a == 2 and b == 18:
+    print("Special")
+else:
+    print("After")
