35fd93d8d34ae52bc9266d2bc5693340dda70b4a
tayo5712 <kimty5712@gmail.com>
2023-06-07 21:14:38 +0900

    boj8979-올림픽

---
+++ Python/baekjoon/no_class/8979-올림픽.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+s = [list(map(int, input().split())) for _ in range(n)]
+s.sort(key=lambda x : (x[1], x[2], x[3]), reverse=True)
+idx = 0
+for i in range(n) :
+    if s[i][0] == m:
+       idx = i
+       break
+
+for i in range(n):
+    if s[idx][1:] == s[i][1:]:
+        print(i+1)
+        break
+
