faad6e61073f6d78827e52941a7f57946c7227f8
tayo5712 <kimty5712@gmail.com>
2023-06-29 21:32:20 +0900

    04_단어뒤집기

---
+++ Java/inflearn/_04_단어_뒤집기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class _04_단어_뒤집기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String word = sc.next();
+            // 1
+//            StringBuilder sb = new StringBuilder(word);
+            // String result = new StringBuilder(word).reverse().toString();
+//            System.out.println(sb.reverse());
+
+            // 2
+            char[] cl = word.toCharArray();
+            for (int j = 0; j < cl.length/2; j++) {
+                int pos = cl.length - (j + 1);
+                char tmp = cl[pos];
+                cl[pos] = cl[j];
+                cl[j] = tmp;
+            }
+            String s = String.valueOf(cl);
+            System.out.println(s);
+        }
+    }
+}
