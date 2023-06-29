0c55f866463012a53848702acac4bc9d60f5ce1f
tayo5712 <kimty5712@gmail.com>
2023-06-29 22:18:57 +0900

    05_특정문자뒤집기

---
+++ Java/inflearn/_05_특정_문자_뒤집기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class _05_특정_문자_뒤집기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String words = sc.next();
+        char[] list = words.toCharArray();
+        int lt = 0;
+        int rt = list.length - 1;
+        while (lt < rt) {
+            if (!Character.isLetter(list[lt])) {
+                lt++;
+            } else if (!Character.isLetter(list[rt])) {
+                rt--;
+            } else {
+                char temp = list[rt];
+                list[rt] = list[lt];
+                list[lt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+        System.out.println(String.valueOf(list));
+    }
+}
