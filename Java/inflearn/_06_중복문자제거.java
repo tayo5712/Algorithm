7e801bef92bd9037ef4d5895d6409d8c821971d6
tayo5712 <kimty5712@gmail.com>
2023-06-30 13:41:20 +0900

    06_중복문자제거

---
+++ Java/inflearn/_06_중복문자제거.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class _06_중복문자제거 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String words = sc.next();
+        String result = "";
+        for (int i = 0; i < words.length(); i++) {
+            if (i == words.indexOf(words.charAt(i))) {
+                result += words.charAt(i);
+            }
+        }
+        System.out.println(result);
+
+    }
+}
