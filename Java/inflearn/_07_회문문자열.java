a62888c97a9c5aa8cc00cd3ae44a10f247e53d63
tayo5712 <kimty5712@gmail.com>
2023-07-01 23:55:08 +0900

    07_회문문자열

---
+++ Java/inflearn/_07_회문문자열.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class _07_회문문자열 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.next();
+        StringBuilder sd = new StringBuilder(word1);
+        String word2 = String.valueOf(sd.reverse());
+        if (word1.equalsIgnoreCase(word2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
