85c1908b4c3f905913d0621230a58ce6fc41fb81
tayo5712 <kimty5712@gmail.com>
2023-07-02 23:59:58 +0900

    09_숫자만 추출

---
+++ Java/inflearn/_09_숫자만_추출.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class _09_숫자만_추출 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        String result = "";
+        for (char c : word.toCharArray()) {
+            // 1
+            if (c >= 48 && c <= 57) {
+                result += c;
+            }
+            // 2
+//            if (Character.isDigit(c)) {
+//                result += c;
+//            }
+        }
+        System.out.println(Integer.parseInt(result));
+    }
+}
