dd217a789c90781a4d2f93c088f0edb17c01ebb8
tayo5712 <kimty5712@gmail.com>
2023-07-04 03:36:53 +0900

    02_04_피보나치수열

---
+++ Java/inflearn/Array/_02_04_피보나치수열.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class _02_04_피보나치수열 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
