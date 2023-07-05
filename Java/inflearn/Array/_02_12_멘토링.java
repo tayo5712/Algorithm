e92d4080fb55b400494618a9af7c98b55fc943a7
tayo5712 <kimty5712@gmail.com>
2023-07-06 02:51:12 +0900

    02_12_멘토링

---
+++ Java/inflearn/Array/_02_12_멘토링.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.Scanner;
+
+public class _02_12_멘토링 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < n; s++) {
+                        // (i, j) (멘티, 멘토) 멘티와 멘토의 등수를 구해야함
+                        if (arr[k][s] == i) pi = s;
+                        if (arr[k][s] == j) pj = s;
+                    }
+                    if (pi < pj) cnt++;
+                }
+                if (cnt == m) answer++;
+            }
+        }
+        System.out.println(answer);
+    } 
+}
