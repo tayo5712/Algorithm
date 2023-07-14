28faf886e1e001366b780abf1c6693d760a5203f
tayo5712 <kimty5712@gmail.com>
2023-07-14 20:45:59 +0900

    boj11256-사탕

---
+++ Python/baekjoon/no_class/11256-사탕.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    J, N = map(int, input().split())
+    cases = []
+    for i in range(N):
+        R, C = map(int, input().split())
+        cases.append(R*C)
+    cases.sort(reverse=True)
+    cnt = 0
+    for case in cases:
+        J -= case
+        cnt += 1
+        if J <= 0:
+            print(cnt)
+            break
