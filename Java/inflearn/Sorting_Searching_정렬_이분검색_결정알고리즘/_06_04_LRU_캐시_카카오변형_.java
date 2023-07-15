760f5a309b0b199afc2f7f33fc954678de9c999d
tayo5712 <kimty5712@gmail.com>
2023-07-15 22:34:13 +0900

    LRU_캐시(카카오변형)

---
+++ Java/inflearn/Sorting_Searching_정렬_이분검색_결정알고리즘/_06_04_LRU_캐시_카카오변형_.java
@@ -0,0 +1,40 @@
+package Sorting_Searching_정렬_이분검색_결정알고리즘;
+
+import java.util.*;
+
+public class _06_04_LRU_캐시_카카오변형_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] cache = new int[s];
+        for (int x : arr) {
+            int pos = -1;
+            for (int i = 0; i < s; i++) {
+                if (x == cache[i]) {
+                    pos = i;
+                }
+            }
+            if (pos == -1) {
+                for (int i = s - 1; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+            }
+            else {
+                for (int i = pos; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+            }
+            cache[0] = x;
+        }
+
+        for (int i : cache) {
+            System.out.print(i + " ");
+        }
+    }
+}
