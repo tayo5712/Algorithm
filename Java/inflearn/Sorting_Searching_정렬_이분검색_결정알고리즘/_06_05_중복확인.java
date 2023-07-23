48921404a9d4ac94d66329d61ed615931abd1402
tayo5712 <kimty5712@gmail.com>
2023-07-23 23:43:21 +0900

    06_05_중복확인

---
+++ Java/inflearn/Sorting_Searching_정렬_이분검색_결정알고리즘/_06_05_중복확인.java
@@ -0,0 +1,21 @@
+package Sorting_Searching_정렬_이분검색_결정알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class _06_05_중복확인 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        if (set.size() == n) System.out.println("U");
+        else System.out.println("D");
+    }
+}
