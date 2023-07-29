fbceb85983edaeb14b24f53f31b8bf303ca5f047
tayo5712 <kimty5712@gmail.com>
2023-07-29 18:27:07 +0900

    boj2644-촌수계산(수정)

--- Python/baekjoon/no_class/2644-촌수계산.py
+++ Python/baekjoon/no_class/2644-촌수계산.py
@@ -1,15 +1,9 @@
 
-def DFS(st, visited, L):
+def DFS(st):
     for k in arr[st]:
         if not visited[k]:
-            if k == y:
-                return L + 1;
-            else:
-                visited[k] = 1
-                result = DFS(k, visited, L + 1)
-                if result != -1:
-                    return result
-    return -1
+            visited[k] = visited[st] + 1
+            DFS(k)
 
 n = int(input())
 x, y = map(int, input().split())
@@ -19,7 +13,6 @@ for _ in range(m):
     a, b = map(int, input().split())
     arr[a].append(b)
     arr[b].append(a)
-
 visited = [0 for _ in range(n+1)]
-visited[x] = 1
-print(DFS(x, visited, 0))
+DFS(x)
+print(visited[y] if visited[y] > 0 else -1)
