91147b3dacbe03bba1df79a9e21ea8508f299b7c
tayo5712 <kimty5712@gmail.com>
2023-08-04 17:06:52 +0900

    boj2559-수열

--- Python/baekjoon/no_class/2559-수열.py
+++ Python/baekjoon/no_class/2559-수열.py
@@ -3,17 +3,30 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
+answer = 0
+curSum = 0
+lt = 0
+for rt in range(m):
+    curSum += arr[rt]
+answer = curSum
+for rt in range(m, n):
+    curSum += arr[rt]
+    curSum -= arr[lt]
+    answer = max(answer, curSum)
+    lt += 1
+print(answer)
+
 # 첫 번째 연속된 값의 합을 구하고
 # 다음 연속된 값의 합
 # = 바로 이전 연속된 온도의 합 - 이전 연속 된 날짜 중 바로 앞 날짜(전날)의 온도 + 다음 연속된 값 에서의 m번째 온도
 
-lst = []
-sumV = sum(arr[:m])
-lst.append(sumV)
-for i in range(1, len(arr)-m+1):
-    sumV = sumV-arr[i-1]+arr[i+m-1]
-    lst.append(sumV)
-print(max(lst))
+# lst = []
+# sumV = sum(arr[:m])
+# lst.append(sumV)
+# for i in range(1, len(arr)-m+1):
+#     sumV = sumV-arr[i-1]+arr[i+m-1]
+#     lst.append(sumV)
+# print(max(lst))
 
 # 시간초과
 # maxV = 0
