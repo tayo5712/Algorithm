0716bd60cba674b4c343162827bc256b3d69cd21
tayo5712 <kimty5712@gmail.com>
2023-08-07 19:44:11 +0900

    swea12222-문자열나누기

--- Java/swea/_12222_문자열나누기.java
+++ Java/swea/_12222_문자열나누기.java
@@ -9,15 +9,16 @@ public class _12222_문자열나누기 {
         int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
             String str = br.readLine();
-            StringBuilder cur = new StringBuilder();
-            StringBuilder pre = new StringBuilder();
+            String cur = "";
+            String pre = "";
             ArrayList<String> arr = new ArrayList<>();
             for (int i = 0; i < str.length(); i++) { // 이전 문자열과 같지 않으면 현재 문자열을 넣는다, 같으면 현재 문자열에 계속 더해주기
-                cur.append(str.charAt(i));
-                if (!pre.toString().equals(cur.toString())) {
-                    arr.add(String.valueOf(cur));
-                    pre = new StringBuilder(cur);
-                    cur.setLength(0);
+                cur += str.charAt(i);
+                if (!pre.equals(cur)) {
+                    arr.add(cur);
+                    pre = cur;
+                    cur = "";
+                    System.out.println(pre + " " + cur);
                 }
             }
             System.out.println("#" + tc + " " + arr.size());
--- Java/swea/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        StringBuilder a = new StringBuilder();
-        System.out.println(a.toString());
-
-    }
-}
