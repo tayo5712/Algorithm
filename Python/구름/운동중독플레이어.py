3b41b3d0e9ac220b0a3993f162336dafa5be6e8b
tayo5712 <kimty5712@gmail.com>
2023-08-14 14:43:33 +0900

    goorm_운동중독플레이어

---
+++ Python/구름/운동중독플레이어.py
@@ -0,0 +1,5 @@
+# https://level.goorm.io/exam/195683/%EC%9A%B4%EB%8F%99-%EC%A4%91%EB%8F%85-%ED%94%8C%EB%A0%88%EC%9D%B4%EC%96%B4/quiz/1
+# 구름 챌린지 1일차
+
+W, R = map(int, input().split())
+print(int(W * (1 + (R/30))))
