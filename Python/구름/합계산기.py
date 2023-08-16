fb7ff77d3238e8d1e04924a7a696143ddb032e2f
tayo5712 <kimty5712@gmail.com>
2023-08-16 12:56:17 +0900

    goorm_합계산기

---
+++ Python/구름/합계산기.py
@@ -0,0 +1,8 @@
+# https://level.goorm.io/exam/195685/%ED%95%A9-%EA%B3%84%EC%82%B0%EA%B8%B0/quiz/1
+# 구름 챌린지 3일차
+
+n = int(input())
+answer = 0
+for _ in range(n):
+	answer += int(eval(input()))
+print(answer)
