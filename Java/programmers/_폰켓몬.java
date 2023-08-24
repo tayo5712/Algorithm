22098f7ddd3f1e4baf0129fdcd5334101db235da
tayo5712 <kimty5712@gmail.com>
2023-08-25 01:48:38 +0900

    pro_폰켓몬

---
+++ Java/programmers/_폰켓몬.java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+public class _폰켓몬 {
+    public int solution(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) hs.add(nums[i]);
+        if (hs.size() < nums.length / 2) return hs.size();
+        else return (nums.length / 2);
+    }
+}
