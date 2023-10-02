00f5848fb09ab7959a6575bbb8a394645784eec6
tayo5712 <kimty5712@gmail.com>
2023-10-02 22:46:18 +0900

    inf_02_10_복습

---
+++ Java/inflearn/Array/_02_10_봉우리_복습.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _02_10_봉우리_복습 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n + 2][n + 2];
+        for (int i = 1; i < n + 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < n + 1; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int answer = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (arr[i][j] > arr[i-1][j] &&
+                        arr[i][j] > arr[i+1][j] &&
+                        arr[i][j] > arr[i][j-1] &&
+                        arr[i][j] > arr[i][j+1]) {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
