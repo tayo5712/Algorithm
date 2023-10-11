2772d02ed56267dd8d2e48be1f5216a4c70633e4
tayo5712 <kimty5712@gmail.com>
2023-10-11 21:36:22 +0900

    boj1965_상자넣기

---
+++ Python/baekjoon/no_class/1965_상자넣기.py
@@ -0,0 +1,8 @@
+n = int(input())
+lst = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
+for i in range(1, n):
+    for j in range(i-1, -1, -1):
+        if lst[j] < lst[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
