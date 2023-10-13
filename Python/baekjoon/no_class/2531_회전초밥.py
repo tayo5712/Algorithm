2ddb5574ad5ec13b7c9ac3bc09fe7221d9141572
tayo5712 <kimty5712@gmail.com>
2023-10-13 23:46:12 +0900

    2531_회전초밥

---
+++ Python/baekjoon/no_class/2531_회전초밥.py
@@ -0,0 +1,20 @@
+n, d, k, c = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+lp, rp = 0, 0
+answer = 0
+
+while lp != n:
+    rp = lp + k
+    case = set()
+    addable = True
+    for i in range(lp, rp):
+        i %= n
+        case.add(arr[i])
+        if arr[i] == c: addable = False
+
+    cnt = len(case)
+    if addable: cnt += 1
+    answer = max(answer, cnt)
+    lp += 1
+
+print(answer)
