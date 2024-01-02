45dae64a088435258b584b67562f3534ca5220ed
tayo <73647852+tayo5712@users.noreply.github.com>
2024-01-02 09:47:53 +0900

    boj2776_암기왕.py

---
+++ Python/baekjoon/no_class/2776_암기왕.py
@@ -0,0 +1,11 @@
+tc = int(input())
+for _ in range(tc):
+    n = int(input())
+    note1 = set(map(int, input().split()))
+    m = int(input())
+    note2 = list(map(int, input().split()))
+    for num in note2:
+        if num in note1:
+            print(1)
+        else:
+            print(0) 
