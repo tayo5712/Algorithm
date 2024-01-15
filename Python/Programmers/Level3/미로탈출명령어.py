d46b216d8e98fd916731be46a0b9c156cfd73ccb
tayo <73647852+tayo5712@users.noreply.github.com>
2024-01-15 13:54:28 +0900

    boj미로탈출명령어.py

--- Python/Programmers/Level3/미로탈출명령어.py
+++ Python/Programmers/Level3/미로탈출명령어.py
@@ -32,3 +32,39 @@ def solution(n, m, x, y, r, c, k):
 
     return bfs()
 
+'''
+O(N) 풀이
+
+def solution(n, m, x, y, r, c, k):
+    answer = ''
+    diff = abs(x-r)+abs(y-c)
+    if diff % 2 != k % 2 or diff > k:
+        return 'impossible'
+    #dlru 순서
+
+    rest = k - diff
+    lcount = 0
+    rcount = 0
+    dcount = 0
+    ucount = 0
+    if x < r : #내려가야함
+        dcount = r - x
+    else:
+        ucount = x - r
+    if y < c :
+        rcount = c - y
+    else:
+        lcount = y - c
+
+    dplus = min(n-max(x,r), rest//2)
+    rest -= dplus * 2
+
+    lplus = min(min(y,c)-1, rest//2)
+    rest -= lplus * 2
+
+    answer = 'd' * (dcount+dplus) + 'l' * (lcount+lplus) + 'rl' * (rest//2) + 'r' * (rcount+lplus) + 'u' * (dplus+ucount)
+    print(lcount, lplus, rcount, rest)
+
+    return answer
+
+'''
