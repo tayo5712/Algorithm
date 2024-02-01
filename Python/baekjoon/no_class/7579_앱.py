97dfbe49d5ca80a659b177630e925b928b6f5c7b
tayo <73647852+tayo5712@users.noreply.github.com>
2024-02-01 19:44:16 +0900

    boj7579_앱.py

---
+++ Python/baekjoon/no_class/7579_앱.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+memories = [0] + list(map(int, input().split()))
+costs = [0] + list(map(int, input().split()))
+dp = [[0 for _ in range(sum(costs) + 1)] for _ in range(n + 1)]
+
+result = sum(costs)
+
+for i in range(1, n + 1):
+    memory = memories[i]
+    cost = costs[i]
+    for j in range(sum(costs) + 1):
+        if cost > j:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost] + memory)
+
+        if dp[i][j] >= m:
+            result = min(result, j)
+
+print(result)
