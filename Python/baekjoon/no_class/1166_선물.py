657f7fa5a4de77dfdb4042e96f8a0aa28b7824c0
tayo <73647852+tayo5712@users.noreply.github.com>
2024-05-07 19:58:29 +0900

    boj1166_선물.py

---
+++ Python/baekjoon/no_class/1166_선물.py
@@ -0,0 +1,10 @@
+N, L, W, H = map(int, input().split())
+s, e = 0, max(L, W, H)
+for _ in range(100):
+	m = (s + e) / 2
+	if (L // m) * (W // m) * (H // m) >= N:
+		s = m
+	else:
+		e = m
+
+print(f'{m:.10f}')
