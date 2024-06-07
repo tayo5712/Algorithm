6f61362c2367fc846aaf36d7b25b7024bf8dd545
tayo <73647852+tayo5712@users.noreply.github.com>
2024-06-07 17:23:17 +0900

    pro_큰수만들기.py

---
+++ Python/Programmers/Level2/큰수만들기.py
@@ -0,0 +1,10 @@
+
+def solution(number, k):
+    stack = []
+    for num in number:
+        while stack and k > 0 and stack[-1] < num:
+            stack.pop()
+            k -= 1
+        stack.append(num)
+        stack = stack[::-k]
+    return ''.join(stack)
