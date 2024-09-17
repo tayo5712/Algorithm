419f1ef7826613ed01fd6de68b10666e0e56b91c
tayo <73647852+tayo5712@users.noreply.github.com>
2024-09-17 14:13:51 +0900

    boj14929_귀찮아(SIB).py

---
+++ Python/baekjoon/no_class/14929_귀찮아(SIB).py
@@ -0,0 +1,9 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+prefix_sum = 0
+res = 0
+for i in range(len(nums) - 1):
+    prefix_sum += nums[i]
+    res += nums[i + 1] * prefix_sum
+print(res)
