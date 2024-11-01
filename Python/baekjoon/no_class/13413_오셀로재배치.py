6a73af1012cf7f29cb74787831c1254472b025a0
tayo <73647852+tayo5712@users.noreply.github.com>
2024-11-01 18:27:20 +0900

    boj13413_오셀로재배치.py

---
+++ Python/baekjoon/no_class/13413_오셀로재배치.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T) :
+    n = int(input())
+    
+    cnt_b = 0
+    cnt_w = 0
+    data1 = input()
+    data2 = input()
+    for i in range(n):
+        if data1[i] != data2[i]:
+            if data1[i] == 'B':
+                cnt_b += 1
+            else:
+                cnt_w += 1
+    print(max(cnt_b, cnt_w))
